14aa825e74c471b60443659fbab15d2294948b12
Valentin Kolesov <vkolesov@rixtrema.com>
2021-05-25 15:18:32 +0700

    PayloadPosts_fts

--- src/pocketdb/docs/main.sql
+++ src/pocketdb/docs/main.sql
@@ -555,3 +555,27 @@ where f.Name match 'loki*' and f.TxHash=wu.Hash
 limit 60;
 */
 
+/*
+DROP TABLE PayloadPosts_fts;
+CREATE VIRTUAL TABLE PayloadPosts_fts USING fts5(
+    TxHash UNINDEXED,
+    Caption,
+    Message,
+    Tags
+);
+--update Payload set String2='brangr' where TxHash='eb7e1228f8c099afb8d3361de8be7f01fb96bf62b5063c90af0419ddefc8079b'
+insert into PayloadPosts_fts
+select
+       t.Hash,
+       p.String2 as Caption,
+       p.String3 as Message,
+       p.String4 as Tags
+from Transactions t
+join Payload p on t.Hash = p.TxHash
+where t.Type = 200;
+
+
+select wu.* from PayloadPosts_fts f, vWebPosts wu
+where f.message match 'Trump' and f.TxHash=wu.Hash
+limit 60;*/
+
