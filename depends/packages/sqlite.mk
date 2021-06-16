648ab57695a667c877ac915f56bb22c21c2415cc
Pocketnet Team <support@pocketnet.app>
2021-06-16 13:53:11 +0300

    upgrade sqlite3 package depends

--- depends/packages/sqlite.mk
+++ depends/packages/sqlite.mk
@@ -1,8 +1,8 @@
 package=sqlite
-$(package)_version=3320100
-$(package)_download_path=https://sqlite.org/2020/
+$(package)_version=3350500
+$(package)_download_path=https://sqlite.org/2021/
 $(package)_file_name=sqlite-autoconf-$($(package)_version).tar.gz
-$(package)_sha256_hash=486748abfb16abd8af664e3a5f03b228e5f124682b0c942e157644bf6fff7d10
+$(package)_sha256_hash=f52b72a5c319c3e516ed7a92e123139a6e87af08a2dc43d7757724f6132e6db0
 
 define $(package)_set_vars
 $(package)_config_opts=--disable-shared --disable-readline --disable-dynamic-extensions --enable-option-checking
