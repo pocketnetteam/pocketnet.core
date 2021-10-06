31f1d6a01568344bfe10843d7aead5af582eed72
lostystyg <andrew300399@gmail.com>
2021-10-06 18:14:51 +0300

    crc32 and crypto

--- src/crypto/sha1.h
+++ src/crypto/sha1.h
@@ -1,9 +1,9 @@
-// Copyright (c) 2014-2016 The Pocketcoin Core developers
+// Copyright (c) 2014-2016 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef POCKETCOIN_CRYPTO_SHA1_H
-#define POCKETCOIN_CRYPTO_SHA1_H
+#ifndef BITCOIN_CRYPTO_SHA1_H
+#define BITCOIN_CRYPTO_SHA1_H
 
 #include <stdint.h>
 #include <stdlib.h>
@@ -25,4 +25,4 @@ public:
     CSHA1& Reset();
 };
 
-#endif // POCKETCOIN_CRYPTO_SHA1_H
+#endif // BITCOIN_CRYPTO_SHA1_H
