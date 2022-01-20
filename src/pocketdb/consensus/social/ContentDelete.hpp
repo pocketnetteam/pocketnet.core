9c15d784a2f40877495b105d2f787543579eeade
Andy Oknen <andy.oknen@yandex.ru>
2022-01-20 15:22:01 +0300

    Add Article to Consensus validating logic

--- src/pocketdb/consensus/social/ContentDelete.hpp
+++ src/pocketdb/consensus/social/ContentDelete.hpp
@@ -29,7 +29,7 @@ namespace PocketConsensus
             // Actual content not deleted
             auto[ok, actuallTx] = ConsensusRepoInst.GetLastContent(
                 *ptx->GetRootTxHash(),
-                { CONTENT_POST, CONTENT_VIDEO, CONTENT_DELETE }
+                { CONTENT_POST, CONTENT_VIDEO, CONTENT_ARTICLE, CONTENT_DELETE }
             );
 
             if (!ok)
@@ -62,7 +62,7 @@ namespace PocketConsensus
         {
             for (auto& blockTx : *block)
             {
-                if (!TransactionHelper::IsIn(*blockTx->GetType(), {CONTENT_POST, CONTENT_VIDEO, CONTENT_DELETE}))
+                if (!TransactionHelper::IsIn(*blockTx->GetType(), {CONTENT_POST, CONTENT_VIDEO, CONTENT_ARTICLE, CONTENT_DELETE}))
                     continue;
 
                 if (*blockTx->GetHash() == *ptx->GetHash())
