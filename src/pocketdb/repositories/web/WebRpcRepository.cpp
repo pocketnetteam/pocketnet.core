530cfe2f807f59595882628d893fa10da061ee73
Andy Oknen <andy.oknen@yandex.ru>
2022-04-04 09:53:37 +0300

    Fix missed comment answers

--- src/pocketdb/repositories/web/WebRpcRepository.cpp
+++ src/pocketdb/repositories/web/WebRpcRepository.cpp
@@ -2008,13 +2008,13 @@ namespace PocketDb
 
         string sql = R"sql(
             select
-                c.String2 as RootTxHash,
-                c.Time,
-                c.Height,
-                c.String1 as addrFrom,
-                c.String3 as posttxid,
-                c.String4 as  parentid,
-                c.String5 as  answerid
+                a.String2 as RootTxHash,
+                a.Time,
+                a.Height,
+                a.String1 as addrFrom,
+                a.String3 as posttxid,
+                a.String4 as parentid,
+                a.String5 as answerid
             from Transactions c indexed by Transactions_Type_Last_String1_String2_Height
             join Transactions a indexed by Transactions_Type_Last_Height_String5_String1
                 on a.Type in (204, 205) and a.Height > ? and a.Last = 1 and a.String5 = c.String2 and a.String1 != c.String1
