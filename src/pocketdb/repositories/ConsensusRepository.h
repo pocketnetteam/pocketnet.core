e77350a99d84626c38dd62338bef217feeb22d79
Andy Oknen <andy.oknen@yandex.ru>
2022-04-11 19:36:04 +0300

    Exclude deleted content for check ModerationFlag

--- src/pocketdb/repositories/ConsensusRepository.h
+++ src/pocketdb/repositories/ConsensusRepository.h
@@ -82,7 +82,7 @@ namespace PocketDb
         bool ExistsUserRegistrations(vector<string>& addresses, bool mempool);
         bool ExistsAnotherByName(const string& address, const string& name);
         bool Exists(const string& txHash, const vector<TxType>& types, bool inChain);
-        bool Exists(const string& txHash, const string& address, const vector<TxType>& types, bool inChain);
+        bool ExistsNotDeleted(const string& txHash, const string& address, const vector<TxType>& types);
 
         // get counts in "mempool" - Height is null
         int CountMempoolBlocking(const string& address, const string& addressTo);
