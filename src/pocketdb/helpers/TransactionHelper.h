303d865373f3b406a9bf9613d9fb9355a29ec14b
only1question <only1question@protonmail.com>
2022-05-06 17:27:36 +0300

    Added TODO flags for future fee changes

--- src/pocketdb/helpers/TransactionHelper.h
+++ src/pocketdb/helpers/TransactionHelper.h
@@ -73,6 +73,7 @@ namespace PocketHelpers
         static bool IsPocketTransaction(const CTransactionRef& tx, TxType& txType);
         static bool IsPocketTransaction(const CTransactionRef& tx);
         static bool IsPocketTransaction(const CTransaction& tx);
+        static bool IsPocketNeededPaymentTransaction(const CTransactionRef& tx);
         static tuple<bool, shared_ptr<ScoreDataDto>> ParseScore(const CTransactionRef& tx);
         static PTransactionRef CreateInstance(TxType txType);
         static PTransactionRef CreateInstance(TxType txType, const CTransactionRef& tx);
