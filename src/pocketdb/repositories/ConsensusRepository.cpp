261d7abcec335c7851a7ba65484c967e1b4afaf5
Andy Oknen <andy.oknen@yandex.ru>
2022-05-10 09:54:15 +0300

    Debug sql

--- src/pocketdb/repositories/ConsensusRepository.cpp
+++ src/pocketdb/repositories/ConsensusRepository.cpp
@@ -791,6 +791,8 @@ namespace PocketDb
             TryBindStatementText(stmt, i++, scoreData->ScoreTxHash);
             TryBindStatementText(stmt, i++, scoreData->ContentAddressHash);
 
+            LogPrintf("GetScoreContentCount: %s\n", sqlite3_expanded_sql(*stmt));
+
             if (sqlite3_step(*stmt) == SQLITE_ROW)
                 if (auto[ok, value] = TryGetColumnInt(*stmt, 0); ok)
                     result = value;
@@ -844,6 +846,8 @@ namespace PocketDb
             TryBindStatementInt64(stmt, i++, (int64_t) scoreData->ScoreTime - scoresOneToOneDepth);
             TryBindStatementText(stmt, i++, scoreData->ScoreTxHash);
             TryBindStatementText(stmt, i++, scoreData->ContentAddressHash);
+
+            LogPrintf("GetScoreCommentCount: %s\n", sqlite3_expanded_sql(*stmt));
 
             if (sqlite3_step(*stmt) == SQLITE_ROW)
                 if (auto[ok, value] = TryGetColumnInt(*stmt, 0); ok)
