19a8d3113ff45560026f1d18fc6b76188636b7a8
Andy Oknen <andy.oknen@yandex.ru>
2022-06-01 01:51:41 +0300

    BCLog::MIGRATION - Logging for database migration scripts
    (cherry picked from commit 5604db6388504c2a2dfef484113bf111c8d456f7)

--- src/pocketdb/SQLiteDatabase.cpp
+++ src/pocketdb/SQLiteDatabase.cpp
@@ -133,7 +133,9 @@ namespace PocketDb
 
             while ((pos = sql.find(';')) != std::string::npos)
             {
-                token = sql.substr(0, pos);
+                token = sql.substr(1, pos);
+
+                LogPrint(BCLog::MIGRATION, "Migration Sqlite database `%s` structure..\n---\n%s\n---\n", m_file_path, token);
 
                 LogPrint(BCLog::MIGRATION, "Migration Sqlite database `%s` structure..\n---\n%s\n---\n", m_file_path, token);
 
