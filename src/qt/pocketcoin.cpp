a0d62b05f8cc99e5be7c62c7b13c4ade87c8503c
lostystyg <andrew300399@gmail.com>
2022-06-18 14:05:17 +0300

    Fixed paths with cyrillics on windows by porting bitcoin changes https://github.com/bitcoin/bitcoin/issues/13869

--- src/qt/pocketcoin.cpp
+++ src/qt/pocketcoin.cpp
@@ -632,6 +632,10 @@ static void SetupUIArgs()
 #ifndef POCKETCOIN_QT_TEST
 int main(int argc, char *argv[])
 {
+#ifdef WIN32
+    util::WinCmdLineArgs winArgs;
+    std::tie(argc, argv) = winArgs.get();
+#endif
     SetupEnvironment();
 
     std::unique_ptr<interfaces::Node> node = interfaces::MakeNode();
