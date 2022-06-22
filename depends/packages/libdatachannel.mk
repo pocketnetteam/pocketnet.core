f9751922cc66d002be4d36cb798ae731772dd633
lostystyg <andrew300399@gmail.com>
2022-06-22 11:48:14 +0300

    Added patch to libdatachannel depends to fix windows crosscompilation

--- depends/packages/libdatachannel.mk
+++ depends/packages/libdatachannel.mk
@@ -5,9 +5,11 @@ $(package)_file_name=$(package)-$($(package)_version)-Source.tar.gz
 $(package)_sha256_hash=3c8de85fde74c375fc9acc5e5614567542497d7607d5574ec9ee3cab97587d3f
 $(package)_dependencies=openssl
 $(package)_patches=build_static.patch
+$(package)_patches+=global_definitions.patch
 
 define $(package)_preprocess_cmds
-  patch -p1 < $($(package)_patch_dir)/build_static.patch
+  patch -p1 < $($(package)_patch_dir)/build_static.patch && \
+  patch -p1 < $($(package)_patch_dir)/global_definitions.patch
 endef
 
 define $(package)_config_cmds
