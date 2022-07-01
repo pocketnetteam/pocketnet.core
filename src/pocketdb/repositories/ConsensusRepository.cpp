46a649c915167581c2e188a9791d85290a97aec6
Andy Oknen <andy.oknen@yandex.ru>
2022-07-01 11:15:53 +0300

    Add height argument for rest emission method

--- src/pocketdb/repositories/ConsensusRepository.cpp
+++ src/pocketdb/repositories/ConsensusRepository.cpp
@@ -843,8 +843,6 @@ namespace PocketDb
             TryBindStatementText(stmt, i++, scoreData->ScoreTxHash);
             TryBindStatementText(stmt, i++, scoreData->ContentAddressHash);
 
-            LogPrintf("GetScoreCommentCount: %s\n", sqlite3_expanded_sql(*stmt));
-
             if (sqlite3_step(*stmt) == SQLITE_ROW)
                 if (auto[ok, value] = TryGetColumnInt(*stmt, 0); ok)
                     result = value;
