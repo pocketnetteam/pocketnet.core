1a981c933d9078fdc60b33c6c840131fcd557f5a
lostystyg <andrew300399@gmail.com>
2022-07-25 15:01:51 +0300

    Removed debug logging

--- src/pocketdb/repositories/web/WebRpcRepository.cpp
+++ src/pocketdb/repositories/web/WebRpcRepository.cpp
@@ -5295,7 +5295,6 @@ namespace PocketDb
                 bind(stmt, i, queryParams);
             }
 
-            LogPrintf(sqlite3_expanded_sql(*stmt));
             while (sqlite3_step(*stmt) == SQLITE_ROW)
             {
                 reconstructor.FeedRow(*stmt);
