884893775de6aea42350bddcdd93ec42c47c6d04
lostystyg <andrew300399@gmail.com>
2022-08-24 16:55:14 +0400

    Removed reposts from activities

--- src/pocketdb/helpers/ShortFormHelper.cpp
+++ src/pocketdb/helpers/ShortFormHelper.cpp
@@ -70,7 +70,6 @@ bool PocketHelpers::ShortTxFilterValidator::Activities::IsFilterAllowed(PocketDb
         PocketDb::ShortTxType::CommentScore,
         PocketDb::ShortTxType::ContentScore,
         PocketDb::ShortTxType::Boost,
-        PocketDb::ShortTxType::Repost
     };
 
     return allowed.find(type) != allowed.end();
--- src/pocketdb/web/PocketContentRpc.cpp
+++ src/pocketdb/web/PocketContentRpc.cpp
@@ -1471,7 +1471,6 @@ namespace PocketWeb::PocketWebRpc
                                 {ShortTxTypeConvertor::toString(ShortTxType::CommentScore), RPCArg::Type::STR, RPCArg::Optional::OMITTED_NAMED_ARG, "acc's comments scores"},
                                 {ShortTxTypeConvertor::toString(ShortTxType::ContentScore), RPCArg::Type::STR, RPCArg::Optional::OMITTED_NAMED_ARG, "acc's content scores"},
                                 {ShortTxTypeConvertor::toString(ShortTxType::Boost), RPCArg::Type::STR, RPCArg::Optional::OMITTED_NAMED_ARG, "boosts content done by acc"},
-                                {ShortTxTypeConvertor::toString(ShortTxType::Repost), RPCArg::Type::STR, RPCArg::Optional::OMITTED_NAMED_ARG, "reposts done by acc"},
                             }
                         }
                           },
