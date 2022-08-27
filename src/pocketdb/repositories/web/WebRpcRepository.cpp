d4582bbf4d15b28f48cf1926b4bf886ca834708b
Andy Oknen <andy.oknen@yandex.ru>
2022-08-27 12:27:00 +0300

    Remove debug logging

--- src/pocketdb/repositories/web/WebRpcRepository.cpp
+++ src/pocketdb/repositories/web/WebRpcRepository.cpp
@@ -5433,8 +5433,7 @@ namespace PocketDb
                     and r.Hash = r.String2 -- Only orig
                     and r.Height = ?
                     and r.String3 is not null
-
-        )sql",
+            )sql",
             heightBinder
         }}
         };
@@ -5452,11 +5451,8 @@ namespace PocketDb
                     int i = 1;
                     selectData.binding(stmt, i, queryParams);
 
-                    LogPrintf(sqlite3_expanded_sql(*stmt));
                     while (sqlite3_step(*stmt) == SQLITE_ROW)
-                    {
                         reconstructor.FeedRow(*stmt);
-                    }
 
                     FinalizeSqlStatement(*stmt);
                 });
