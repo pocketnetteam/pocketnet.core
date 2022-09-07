6d1975a22e50550ff6ef7b393441e49f2cadbfe4
Andy Oknen <andy.oknen@yandex.ru>
2022-09-07 10:40:06 +0300

    Disable enabling txindex by default

--- src/init.cpp
+++ src/init.cpp
@@ -2127,9 +2127,12 @@ bool AppInitMain(const util::Ref& context, NodeContext& node, interfaces::BlockA
     fFeeEstimatesInitialized = true;
     
     // ********************************************************* Step 8: start indexers
-    // TODO (brangr): maybe not needed?
-    g_txindex = MakeUnique<TxIndex>(nTxIndexCache, false, fReindex);
-    g_txindex->Start();
+    // TODO (brangr): 0.21.0 check need txindex test
+    if (args.GetBoolArg("-txindex", DEFAULT_TXINDEX))
+    {
+        g_txindex = MakeUnique<TxIndex>(nTxIndexCache, false, fReindex);
+        g_txindex->Start();
+    }
 
     for (const auto& filter_type : g_enabled_filter_types) {
         InitBlockFilterIndex(filter_type, filter_index_cache, false, fReindex);
