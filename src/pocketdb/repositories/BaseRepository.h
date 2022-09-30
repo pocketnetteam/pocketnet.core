5d461f47d2a4bfdb58caee5c7371417610cdd9f0
Andy Oknen <andy.oknen@yandex.ru>
2022-09-30 18:17:23 +0300

    Fix loop include for RowAccessor

--- src/pocketdb/repositories/BaseRepository.h
+++ src/pocketdb/repositories/BaseRepository.h
@@ -10,6 +10,7 @@
 #include "shutdown.h"
 #include "pocketdb/SQLiteDatabase.h"
 #include "pocketdb/helpers/TransactionHelper.h"
+#include "pocketdb/repositories/RowAccessor.hpp"
 
 #include <boost/algorithm/string/replace.hpp>
 
@@ -17,31 +18,6 @@ namespace PocketDb
 {
     using namespace std;
     using namespace PocketHelpers;
-
-    class RowAccessor
-    {
-    public:
-        tuple<bool, std::string> TryGetColumnString(sqlite3_stmt* stmt, int index)
-        {
-            return sqlite3_column_type(stmt, index) == SQLITE_NULL
-                   ? make_tuple(false, "")
-                   : make_tuple(true, std::string(reinterpret_cast<const char*>(sqlite3_column_text(stmt, index))));
-        }
-
-        tuple<bool, int64_t> TryGetColumnInt64(sqlite3_stmt* stmt, int index)
-        {
-            return sqlite3_column_type(stmt, index) == SQLITE_NULL
-                   ? make_tuple(false, (int64_t) 0)
-                   : make_tuple(true, (int64_t) sqlite3_column_int64(stmt, index));
-        }
-
-        tuple<bool, int> TryGetColumnInt(sqlite3_stmt* stmt, int index)
-        {
-            return sqlite3_column_type(stmt, index) == SQLITE_NULL
-                   ? make_tuple(false, 0)
-                   : make_tuple(true, sqlite3_column_int(stmt, index));
-        }
-    };
 
     class BaseRepository : protected RowAccessor
     {
