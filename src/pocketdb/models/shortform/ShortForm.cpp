426b165ced8e305a516ca204b145e361f9657909
Pocketnet Team <support@pocketnet.app>
2022-11-09 12:24:43 +0300

    Update ShortForm.cpp

--- src/pocketdb/models/shortform/ShortForm.cpp
+++ src/pocketdb/models/shortform/ShortForm.cpp
@@ -2,6 +2,7 @@
 // Distributed under the Apache 2.0 software license, see the accompanying
 // https://www.apache.org/licenses/LICENSE-2.0
 
+#include "pocketdb/models/shortform/ShortForm.h"
 #include "pocketdb/helpers/ShortFormHelper.h"
 
 PocketDb::ShortForm::ShortForm(PocketDb::ShortTxType type, ShortTxData txData, std::optional<ShortTxData> relatedContent)
