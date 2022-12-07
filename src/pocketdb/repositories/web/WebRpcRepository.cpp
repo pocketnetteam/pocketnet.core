7b6bbc681ff8b4f74fb200a11a68a82a4ea0e365
Andy Oknen <andy.oknen@yandex.ru>
2022-12-07 11:33:22 +0300

    reafctor

--- src/pocketdb/repositories/web/WebRpcRepository.cpp
+++ src/pocketdb/repositories/web/WebRpcRepository.cpp
@@ -3,11 +3,7 @@
 // https://www.apache.org/licenses/LICENSE-2.0
 
 #include "pocketdb/repositories/web/WebRpcRepository.h"
-
 #include "pocketdb/helpers/ShortFormRepositoryHelper.h"
-
-#include <functional>
-
 #include <functional>
 
 namespace PocketDb
