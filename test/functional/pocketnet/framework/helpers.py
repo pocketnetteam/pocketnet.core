bdf26362854622cbe3a29519a877aca219c15488
zedxter <digaleev@gmail.com>
2023-01-20 00:55:23 +0100

    Update copyright year to 2023 only

--- test/functional/pocketnet/framework/helpers.py
+++ test/functional/pocketnet/framework/helpers.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2018-2023 The Pocketnet developers
+# Copyright (c) 2023 The Pocketnet developers
 # Distributed under the Apache 2.0 software license, see the accompanying
 # https://www.apache.org/licenses/LICENSE-2.0
 
