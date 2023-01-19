5116818a67fe140d344c290b7088e02f151e28fd
lostystyg <andrew300399@gmail.com>
2023-01-19 18:36:42 +0400

    Specific BoostDataTranslator for calculating Int1 before inserting transactions

--- src/pocketdb/helpers/DbViewHelper.cpp
+++ src/pocketdb/helpers/DbViewHelper.cpp
@@ -6,6 +6,7 @@
 
 #include "pocketdb/pocketnet.h"
 
+#include <algorithm>
 
 class CommonDataTranslator : public PocketHelpers::ITxDbDataTranslator
 {
@@ -63,6 +64,51 @@ public:
     }
 };
 
+class BoostDataTranslator : public PocketHelpers::ITxDbDataTranslator
+{
+public:
+    bool Inject(PocketHelpers::PTransactionRef& tx, const PocketHelpers::TxContextualData& data) override
+    {
+        if (data.string1) tx->SetString1(*data.string1);
+        if (data.string2) tx->SetString1(*data.string2);
+        if (data.string4) tx->SetString1(*data.string4);
+        if (data.string5) tx->SetString1(*data.string5);
+        if (data.list) tx->SetString3(*data.list);
+        if (data.int1) tx->SetInt1(*data.int1);
+
+        return true;
+    };
+
+    bool Extract(PocketHelpers::TxContextualData& data, const PocketHelpers::PTransactionRef& tx) override
+    {
+        data.string1 = tx->GetString1();
+        data.string2 = tx->GetString2();
+        data.list = tx->GetString3();
+        data.string4 = tx->GetString4();
+        data.string5 = tx->GetString5();
+        data.int1 = tx->GetInt1().value_or(calculateIntField(tx->Outputs(), tx->Inputs()));
+
+        return true;
+    }
+protected:
+    // TODO (optimizations): find a better place for this maybe (calculate on tx deserialization or smth)
+    int64_t calculateIntField(const std::vector<PocketTx::TransactionOutput>& outputs, const std::vector<PocketTx::TransactionInput>& inputs)
+    {
+        return 
+            std::accumulate(
+                outputs.begin(),
+                outputs.end(),
+                (int64_t)0,
+                [](const int64_t& sum, const PocketTx::TransactionOutput& elem) { return sum + elem.GetValue().value_or(0); }
+            ) - 
+            std::accumulate(
+                inputs.begin(),
+                inputs.end(),
+                (int64_t)0,
+                [](const int64_t& sum, const PocketTx::TransactionInput& elem) { return sum + elem.GetValue().value_or(0); });
+    }
+};
+
 bool PocketHelpers::DbViewHelper::Inject(PTransactionRef& tx, const TxContextualData& data)
 {
     if (auto translator = m_translatorSelector.find(*tx->GetType()); translator != m_translatorSelector.end()) {
@@ -84,9 +130,9 @@ bool PocketHelpers::DbViewHelper::Extract(TxContextualData& data, const PTransac
 #define TRANSLATOR(txType, TranslatorName) \
     {PocketTx::TxType::txType, std::make_shared<TranslatorName>()}
 
-// TODO (losty-db): !!! fill for all other models
 const std::map<PocketDb::TxType, std::shared_ptr<PocketHelpers::ITxDbDataTranslator>> PocketHelpers::DbViewHelper::m_translatorSelector = {
     TRANSLATOR(ACTION_BLOCKING, BlockingDataTranslator),
+    TRANSLATOR(BOOST_CONTENT, BoostDataTranslator),
 };
 
 std::shared_ptr<PocketHelpers::ITxDbDataTranslator> const PocketHelpers::DbViewHelper::m_defaultDataTranslator = std::make_shared<CommonDataTranslator>();
