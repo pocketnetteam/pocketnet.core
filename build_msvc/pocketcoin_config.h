88cdf857a0baf1e7399067f0cb0e69facb107afa
Andy Oknen <andy.oknen@yandex.ru>
2023-01-24 10:23:27 +0300

    v0.22.0

--- build_msvc/pocketcoin_config.h
+++ build_msvc/pocketcoin_config.h
@@ -11,7 +11,7 @@
 #define CLIENT_VERSION_MAJOR 0
 
 /* Minor version */
-#define CLIENT_VERSION_MINOR 21
+#define CLIENT_VERSION_MINOR 22
 
 /* Build revision */
 #define CLIENT_VERSION_REVISION 0
@@ -346,7 +346,7 @@
 #define PACKAGE_NAME "Pocketnet Core"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Pocketnet Core 0.21.0"
+#define PACKAGE_STRING "Pocketnet Core 0.22.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "pocketcoin"
@@ -355,7 +355,7 @@
 #define PACKAGE_URL "https://github.com/pocketnetteam"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.21.0"
+#define PACKAGE_VERSION "0.22.0"
 
 /* Define to necessary symbol if this constant uses a non-standard name on
    your system. */
