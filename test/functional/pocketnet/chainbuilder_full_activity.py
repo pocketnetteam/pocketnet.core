c4df6c145cabb14956dda66d7267e823df9cae48
zedxter <digaleev@gmail.com>
2023-02-03 15:49:41 +0100

    Add missing files from prev commits

---
+++ test/functional/pocketnet/chainbuilder_full_activity.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# Copyright (c) 2023 The Pocketnet developers
+# Distributed under the Apache 2.0 software license, see the accompanying
+# https://www.apache.org/licenses/LICENSE-2.0
+"""
+This test uses chain_builder in order to generate some
+activity on the chain.
+Launch this from 'test/functional/pocketnet' directory
+"""
+
+import sys
+import pathlib
+
+sys.path.insert(0, str(pathlib.Path(__file__).parent.parent))
+
+from framework.chain_builder import ChainBuilder
+
+from test_framework.test_framework import PocketcoinTestFramework
+
+
+class ChainBuilderFullActivityTest(PocketcoinTestFramework):
+    def set_test_params(self):
+        self.setup_clean_chain = True
+        self.num_nodes = 1
+        self.extra_args = [["-debug=consensus"]]
+
+    def run_test(self):
+        node = self.nodes[0]
+        builder = ChainBuilder(node, self.log)
+        builder.build_full()
+
+        info = node.public().getaddressinfo(builder.node_address)
+        self.log.info(f"Node balance: {info}")
+
+
+if __name__ == "__main__":
+    ChainBuilderFullActivityTest().main()
