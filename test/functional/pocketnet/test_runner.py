6483fc7e1555c267206bc75ad82278dbbc71a21e
zedxter <digaleev@gmail.com>
2023-02-08 21:51:58 +0100

    Add autodiscovery & print test summary

--- test/functional/pocketnet/test_runner.py
+++ test/functional/pocketnet/test_runner.py
@@ -24,27 +24,26 @@ sys.path.insert(0, str(pathlib.Path(__file__).parent.parent))
 from test_runner import TestHandler, TestResult, print_results
 
 
-BASE_SCRIPTS = [
-    "account_delete.py",
-    "badges.py",
-    # 'content_collections.py',
-    "moderation_jury_no_ban.py",
-    "moderation_jury.py",
-    "chainbuilder_full_activity.py",
-]
-
-
 NON_SCRIPTS = [
     # These are python files that live in the functional tests
     # directory, but are not test scripts.
+    "__init__.py",
     "test_runner.py",
 ]
 
-BOLD = ('\033[0m', '\033[1m')
+BOLD = ("\033[0m", "\033[1m")
 
 
-def discover():
-    return BASE_SCRIPTS
+def discover(tests_dir):
+    dir_items = os.listdir(tests_dir)
+    scripts = [f for f in dir_items if os.path.isfile(f) and f.endswith(".py")]
+    tests = sorted(list(set(scripts) - set(NON_SCRIPTS)))
+    print("\nDiscovered Tests:\n")
+    for test in tests:
+        print(f"  - {BOLD[1]}{test}{BOLD[0]}")
+    print()
+    return tests
+
 
 def tempdir():
     now = datetime.now().strftime("%Y%m%d_%H%M%S")
@@ -52,11 +51,13 @@ def tempdir():
     os.makedirs(tmpdir)
     return tmpdir
 
+
 def get_config():
     config = configparser.ConfigParser()
     configfile = os.path.abspath(os.path.dirname(__file__)) + "/../../config.ini"
     config.read_file(open(configfile, encoding="utf8"))
     return config
+
 
 def execute(job_queue, test_num, test_count):
     test_result, testdir, stdout, stderr = job_queue.get_next()
@@ -67,36 +68,47 @@ def execute(job_queue, test_num, test_count):
         logging.debug("%s skipped" % (done_str))
     else:
         print("%s failed, Duration: %s s\n" % (done_str, test_result.time))
-        print(BOLD[1] + 'stdout:\n' + BOLD[0] + stdout + '\n')
-        print(BOLD[1] + 'stderr:\n' + BOLD[0] + stderr + '\n')
+        print(BOLD[1] + "stdout:\n" + BOLD[0] + stdout + "\n")
+        print(BOLD[1] + "stderr:\n" + BOLD[0] + stderr + "\n")
     return test_result
+
 
 def setup_logging():
     logging_level = logging.DEBUG
-    logging.basicConfig(format='%(message)s', level=logging_level)
+    logging.basicConfig(format="%(message)s", level=logging_level)
 
-def run_tests():
+
+def prepare_job_queue():
     config = get_config()
     src_dir = config["environment"]["SRCDIR"]
-    build_dir = config["environment"]["BUILDDIR"]
+    tests_dir = os.path.join(src_dir, "test/functional/pocketnet/")
+    temp_dir = tempdir()
+    test_list = discover(tests_dir)
 
-    job_queue = TestHandler(
+    return TestHandler(
         num_tests_parallel=3,
-        tests_dir=os.path.join(src_dir, "test/functional/pocketnet/"),
-        tmpdir=tempdir(),
-        test_list=discover(),
+        tests_dir=tests_dir,
+        tmpdir=temp_dir,
+        test_list=test_list,
         flags=[],
         use_term_control=False,
     )
 
+
+def run_tests():
+    results = list()
+    job_queue = prepare_job_queue()
     start_time = time.time()
     test_count = len(job_queue.test_list)
 
-    print("Running Pocketnet tests configured in BASE_SCRIPTS...")
+    print(f"Running Pocketnet tests using {job_queue.num_jobs} workers...\n")
 
     for test_num in range(test_count):
-        result = execute(job_queue, test_num, test_count)
-        print(result, f"Runtime: {time.time() - start_time}")
+        test_result = execute(job_queue, test_num, test_count)
+        results.append(test_result)
+
+    print_results(results, 30, (int(time.time() - start_time)))
+
 
 def main():
     setup_logging()
