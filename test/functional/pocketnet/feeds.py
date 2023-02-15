9ceba2341f80b82bbd299ccc0bfd870766bba870
zedxter <digaleev@gmail.com>
2023-02-15 20:44:44 +0100

    Add missing feed tests

--- test/functional/pocketnet/feeds.py
+++ test/functional/pocketnet/feeds.py
@@ -14,7 +14,7 @@ import pathlib
 sys.path.insert(0, str(pathlib.Path(__file__).parent.parent))
 
 from framework.chain_builder import ChainBuilder
-from framework.helpers import add_content, generate_subscription
+from framework.helpers import add_content, boost_post, comment_post, delete_post, generate_subscription, like_post
 from framework.models import (
     ContentPostPayload,
     ContentVideoPayload,
@@ -64,8 +64,15 @@ class FeedTest(PocketcoinTestFramework):
             self.log.info(f"Check - Number of posts of type {content_type} in historical feed: {len(contents)}")
             assert len(contents) == content_len
 
+    def test_hierarchical_feed(self, builder):
+        self.log.info("Test 2 - Hierarchical feed")
+        feed = builder.node.public().gethierarchicalfeed()
+        contents = feed["contents"]
+        self.log.info(f"Check - Number of posts in hierarchical feed: {len(contents)}")
+        assert len(contents) == 9
+
     def test_profile_feed(self, builder):
-        self.log.info("Test 2 - Profile feed")
+        self.log.info("Test 3 - Profile feed")
         accounts = builder.accounts + builder.moderators
         for account, content_len in zip(accounts, [2, 3, 4, 0]):
             params = DEFAULT_FEED_PARAMS.copy() + [account.Address, "", "", "desc"]
@@ -75,7 +82,7 @@ class FeedTest(PocketcoinTestFramework):
             assert len(contents) == content_len
 
     def test_subscribes_feed_no_subscriptions(self, builder):
-        self.log.info("Test 3 - Subscribes feed: No subscriptions")
+        self.log.info("Test 4 - Subscribes feed: No subscriptions")
         accounts = builder.accounts + builder.moderators
         for account in accounts:
             params = DEFAULT_FEED_PARAMS.copy() + [account.Address]
@@ -85,7 +92,7 @@ class FeedTest(PocketcoinTestFramework):
             assert len(contents) == 0
 
     def test_subscribes_feed_with_subscriptions(self, builder):
-        self.log.info("Test 4 - Subscribes feed: With subscriptions")
+        self.log.info("Test 5 - Subscribes feed: With subscriptions")
         accounts = builder.accounts + builder.moderators
 
         self.log.info("Account 1 subscribes to accounts 2 and 3")
@@ -108,16 +115,95 @@ class FeedTest(PocketcoinTestFramework):
                 assert item["address"] in post_authors
 
     def test_boostfeed(self, builder):
-        self.log.info("Test 5 - Boosted feed")
-        self.log.warning("Not implemented yet")
+        self.log.info("Test 6 - Boosted feed")
+        feed = builder.node.public().getboostfeed()
+        boosts = feed["boosts"]
+        self.log.info(f"Check - Number of posts in boost feed without boosts: {len(boosts)}")
+        assert len(boosts) == 0
+
+        # boost some posts
+        boost_post(builder.node, builder.accounts[0], builder.accounts[0].content[0])
+        boost_post(builder.node, builder.accounts[0], builder.accounts[1].content[0])
+        boost_post(builder.node, builder.accounts[1], builder.accounts[2].content[1])
+
+        feed = builder.node.public().getboostfeed()
+        boosts = feed["boosts"]
+        self.log.info(f"Check - Number of posts in boost feed after boosting: {len(boosts)}")
+        assert len(boosts) == 3
+
+        for boost in boosts:
+            assert boost["txid"] in [
+                builder.accounts[0].content[0],
+                builder.accounts[1].content[0],
+                builder.accounts[2].content[1],
+            ]
 
     def test_topfeed(self, builder):
-        self.log.info("Test 6 - Top feed")
-        self.log.warning("Not implemented yet")
+        self.log.info("Test 7 - Top feed")
+        feed = builder.node.public().gettopfeed()
+        contents = feed["contents"]
+        self.log.info(f"Check - Number of posts in top feed: {len(contents)}")
+        assert len(contents) == 0
+
+        # like some posts
+        like_post(builder.node, builder.accounts[2], builder.accounts[0], builder.accounts[2].content[0], 5)
+        like_post(builder.node, builder.accounts[1], builder.moderators[0], builder.accounts[1].content[2], 5)
+        like_post(builder.node, builder.accounts[2], builder.moderators[0], builder.accounts[2].content[0], 4)
+
+        feed = builder.node.public().gettopfeed()
+        contents = feed["contents"]
+
+        self.log.info(f"Check - Number of posts in top feed after post likes: {len(contents)}")
+        assert len(contents) == 2
+
+        for content, author_id, score in zip(
+            contents, [builder.accounts[2].Address, builder.accounts[1].Address], ["9", "5"]
+        ):
+            assert content["address"] == author_id
+            assert content["scoreSum"] == score
 
     def test_mostcommentedfeed(self, builder):
-        self.log.info("Test 5 - Most commented feed")
-        self.log.warning("Not implemented yet")
+        self.log.info("Test 8 - Most commented feed")
+        feed = builder.node.public().getmostcommentedfeed()
+        contents = feed["contents"]
+        self.log.info(f"Check - Number of posts in most commented feed: {len(contents)}")
+        assert len(contents) == 2  # After liking some posts in previous tests
+
+        # comment some posts
+        comment_post(builder.node, builder.moderators[0], builder.accounts[1].content[2])
+        comment_post(builder.node, builder.accounts[0], builder.accounts[1].content[2])
+        comment_post(builder.node, builder.accounts[2], builder.accounts[1].content[2])
+
+        feed = builder.node.public().getmostcommentedfeed()
+        contents = feed["contents"]
+
+        self.log.info("Check - Sorting and values")
+        assert len(contents) == 2
+
+        for content, author_id, score, comments in zip(
+            contents, [builder.accounts[1].Address, builder.accounts[2].Address], ["5", "9"], [3, 0]
+        ):
+            assert content["address"] == author_id
+            assert content["scoreSum"] == score
+            assert content["comments"] == comments
+
+    def test_feeds_after_deletion(self, builder):
+        self.log.info("Test 9 - Feed after deleting posts")
+        feed = builder.node.public().gethistoricalfeed()
+        contents = feed["contents"]
+        self.log.info(f"Check - Number of posts in historical feed: {len(contents)}")
+        assert len(contents) == 9
+
+        self.log.info("Deleting all posts from all accounts")
+        for account in builder.accounts:
+            for post_id in account.content:
+                delete_post(builder.node, account, post_id)
+
+        feed = builder.node.public().gethistoricalfeed()
+        contents = feed["contents"]
+        self.log.info(f"Check - Number of posts in historical feed: {len(contents)}")
+        assert len(contents) == 0
+        
 
     def run_test(self):
         node = self.nodes[0]
@@ -127,12 +213,14 @@ class FeedTest(PocketcoinTestFramework):
         self.generate_posts(builder)
 
         self.test_historical_feed(builder)
+        self.test_hierarchical_feed(builder)
         self.test_profile_feed(builder)
         self.test_subscribes_feed_no_subscriptions(builder)
         self.test_subscribes_feed_with_subscriptions(builder)
         self.test_boostfeed(builder)
         self.test_topfeed(builder)
         self.test_mostcommentedfeed(builder)
+        self.test_feeds_after_deletion(builder)
 
 
 if __name__ == "__main__":
