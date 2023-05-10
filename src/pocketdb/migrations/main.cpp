26c4320a16aa8bbe8d2a6a0cd6942d706b3a18b8
lostystyg <andrew300399@gmail.com>
2023-05-10 14:58:29 +0400

    Type_RegId1_Time index

--- src/pocketdb/migrations/main.cpp
+++ src/pocketdb/migrations/main.cpp
@@ -412,6 +412,7 @@ namespace PocketDb
             create index if not exists Transactions_Type_RegId2 on Transactions (Type, RegId2);
             create index if not exists Transactions_Type_RegId5_RegId1 on Transactions (Type, RegId5, RegId1);
             create index if not exists Transactions_Type_RegId1_Int1_Time on Transactions (Type, RegId1, Int1, Time);
+            create index if not exists Transactions_Type_RegId1_Time on Transactions (Type, RegId1, Time);
 
             create index if not exists TxInputs_SpentTxId_TxId_Number on TxInputs (SpentTxId, TxId, Number);
 
