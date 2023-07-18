2c34bdbdc98b71a281debab3dcc3cbd10847cafc
Andy Oknen <andy.oknen@yandex.ru>
2023-07-18 15:01:09 +0800

    Fix indexe lost ;

--- src/pocketdb/migrations/main.cpp
+++ src/pocketdb/migrations/main.cpp
@@ -428,7 +428,7 @@ namespace PocketDb
             create index if not exists Transactions_Type_RegId2_RegId1 on Transactions (Type, RegId2, RegId1);
             create index if not exists Transactions_Type_RegId3_RegId1 on Transactions (Type, RegId3, RegId1);
             create index if not exists Transactions_Type_RegId5_RegId1 on Transactions (Type, RegId5, RegId1);
-            create index if not exists Transactions_Type_RegId4_RegId1 on Transactions (Type, RegId4, RegId1)
+            create index if not exists Transactions_Type_RegId4_RegId1 on Transactions (Type, RegId4, RegId1);
             create index if not exists Transactions_Type_RegId1_Int1_Time on Transactions (Type, RegId1, Int1, Time);
             create index if not exists Transactions_Type_RegId1_Time on Transactions (Type, RegId1, Time);
 
