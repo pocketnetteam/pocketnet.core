f981fe63efc06e62cccd529742d075b8f540cc82
Andy Oknen <andy.oknen@yandex.ru>
2023-07-20 15:50:13 +0800

    Added barteron rpc sources

---
+++ src/pocketdb/web/BarteronRpc.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2018-2022 The Pocketnet developers
+// Distributed under the Apache 2.0 software license, see the accompanying
+// https://www.apache.org/licenses/LICENSE-2.0
+
+#include "pocketdb/web/BarteronRpc.h"
+
+namespace PocketWeb::PocketWebRpc
+{
+    
+}
