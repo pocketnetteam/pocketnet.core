9b17b6fb659e85511341c3b742e05d58a1a32555
Andy Oknen <andy.oknen@yandex.ru>
2023-07-31 17:28:10 +0800

    Upgrade package sqlite to 3.42.0

--- depends/packages/sqlite.mk
+++ depends/packages/sqlite.mk
@@ -1,8 +1,8 @@
 package=sqlite
-$(package)_version=3340100
-$(package)_download_path=https://sqlite.org/2021/
+$(package)_version=3420000
+$(package)_download_path=https://sqlite.org/2023/
 $(package)_file_name=sqlite-autoconf-$($(package)_version).tar.gz
-$(package)_sha256_hash=2a3bca581117b3b88e5361d0ef3803ba6d8da604b1c1a47d902ef785c1b53e89
+$(package)_sha256_hash=7abcfd161c6e2742ca5c6c0895d1f853c940f203304a0b49da4e1eca5d088ca6
 
 define $(package)_set_vars
 $(package)_config_opts=--disable-shared --disable-readline --disable-dynamic-extensions --enable-option-checking
