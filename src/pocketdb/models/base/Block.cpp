5d989553af10b877e6961dd2fee5d5d0f2f1570a
Andy Oknen <andy.oknen@yandex.ru>
2025-01-09 16:00:19 +0800

    Add Block and Transaction models with serialization and deserialization methods

---
+++ src/pocketdb/models/base/Block.cpp
@@ -0,0 +1,124 @@
+// Copyright (c) 2018-2022 The Pocketnet developers
+// Distributed under the Apache 2.0 software license, see the accompanying
+// https://www.apache.org/licenses/LICENSE-2.0
+
+#include "pocketdb/models/base/Transaction.h"
+
+namespace PocketTx
+{
+
+    Transaction::Transaction() : Base()
+    {
+    }
+
+    Transaction::Transaction(const CTransactionRef& tx) : Base()
+    {
+        SetHash(tx->GetHash().GetHex());
+        SetTime(tx->nTime);
+    }
+
+    optional<UniValue> Transaction::Serialize() const
+    {
+        // TODO (losty): optional?
+        UniValue result (UniValue::VOBJ);
+        
+        // TODO (aok): remove safe?        
+        result.pushKV("txid", *GetHash());
+        result.pushKV("time", *GetTime());
+        result.pushKV("block", 0);
+        result.pushKV("last", false);
+
+        return result;
+    }
+
+    void Transaction::Deserialize(const UniValue& src) {}
+
+    void Transaction::DeserializeRpc(const UniValue& src) {}
+
+    void Transaction::DeserializePayload(const UniValue& src)
+    {
+        GeneratePayload();
+    }
+
+    const optional<string>& Transaction::GetHash() const { return m_hash; }
+    void Transaction::SetHash(string value) { m_hash = value; }
+    bool Transaction::operator==(const string& hash) const { return m_hash && *m_hash == hash; }
+
+    const optional<TxType>& Transaction::GetType() const { return m_type; }
+    void Transaction::SetType(TxType value) { m_type = value; }
+
+    const optional<int64_t>& Transaction::GetVersion() const { return m_version; }
+    void Transaction::SetVersion(int64_t value) { m_version = value; }
+
+    const optional<int64_t>& Transaction::GetTime() const { return m_time; }
+    void Transaction::SetTime(int64_t value) { m_time = value; }
+
+    const optional<int64_t>& Transaction::GetLocktime() const { return m_locktime; }
+    void Transaction::SetLocktime(int64_t value) { m_locktime = value; }
+
+    const optional<int64_t>& Transaction::GetHeight() const { return m_height; }
+    void Transaction::SetHeight(int64_t value) { m_height = value; }
+
+    const optional<string>& Transaction::GetBlockHash() const { return m_blockhash; }
+    void Transaction::SetBlockHash(string value) { m_blockhash = value; }
+
+    const optional<bool>& Transaction::GetLast() const { return m_last; }
+    void Transaction::SetLast(bool value) { m_last = value; }
+
+    const optional<string>& Transaction::GetString1() const { return m_string1; }
+    void Transaction::SetString1(string value) { m_string1 = value; }
+
+    const optional<string>& Transaction::GetString2() const { return m_string2; }
+    void Transaction::SetString2(string value) { m_string2 = value; }
+
+    const optional<string>& Transaction::GetString3() const { return m_string3; }
+    void Transaction::SetString3(string value) { m_string3 = value; }
+
+    const optional<string>& Transaction::GetString4() const { return m_string4; }
+    void Transaction::SetString4(string value) { m_string4 = value; }
+
+    const optional<string>& Transaction::GetString5() const { return m_string5; }
+    void Transaction::SetString5(string value) { m_string5 = value; }
+
+    const optional<int64_t>& Transaction::GetInt1() const { return m_int1; }
+    void Transaction::SetInt1(int64_t value) { m_int1 = value; }
+
+    const optional<int64_t>& Transaction::GetId() const { return m_id; }
+    void Transaction::SetId(int64_t value) { m_id = value; }
+
+    vector <TransactionInput>& Transaction::Inputs() { return m_inputs; }
+    vector <TransactionOutput>& Transaction::Outputs() { return m_outputs; }
+    const vector <TransactionOutput>& Transaction::OutputsConst() const { return m_outputs; }
+
+    optional<Payload>& Transaction::GetPayload() { return m_payload; }
+    const optional<Payload>& Transaction::GetPayload() const { return m_payload; }
+    void Transaction::SetPayload(Payload value) { m_payload = value; }
+    bool Transaction::HasPayload() const { return m_payload.has_value(); };
+
+    string Transaction::GenerateHash(const string& data) const
+    {
+        unsigned char hash[32] = {};
+        CSHA256().Write((const unsigned char*) data.data(), data.size()).Finalize(hash);
+        CSHA256().Write(hash, 32).Finalize(hash);
+        std::vector<unsigned char> vec(hash, hash + sizeof(hash));
+
+        return HexStr(vec);
+    }
+
+    void Transaction::GeneratePayload()
+    {
+        m_payload = Payload();
+        m_payload->SetTxHash(*GetHash());
+    }
+
+    void Transaction::ClearPayload()
+    {
+        m_payload = nullopt;
+    }
+
+    size_t Transaction::PayloadSize() const
+    {
+        return 0;
+    }
+
+} // namespace PocketTx
---
+++ src/pocketdb/models/base/Block.h
@@ -0,0 +1,127 @@
+// Copyright (c) 2018-2022 The Pocketnet developers
+// Distributed under the Apache 2.0 software license, see the accompanying
+// https://www.apache.org/licenses/LICENSE-2.0
+
+#ifndef POCKETTX_BLOCK_H
+#define POCKETTX_BLOCK_H
+
+#include <string>
+#include <univalue/include/univalue.h>
+#include <utility>
+#include <util/strencodings.h>
+#include <crypto/sha256.h>
+#include <primitives/block.h>
+#include <primitives/transaction.h>
+
+#include "pocketdb/models/base/Payload.h"
+#include "pocketdb/models/base/Transaction.h"
+#include "pocketdb/util/Empty.h"
+
+#include <optional>
+
+namespace PocketTx
+{
+    using namespace std;
+    using namespace PocketUtil;
+
+    class Block : public Base
+    {
+    public:
+        Block();
+        Block(const CBlock& block);
+
+        virtual optional<UniValue> Serialize() const;
+
+        virtual void Deserialize(const UniValue& src);
+        virtual void DeserializeRpc(const UniValue& src);
+        virtual void DeserializePayload(const UniValue& src);
+
+        virtual string BuildHash() = 0;
+
+        const optional<string>& GetHash() const;
+        void SetHash(string value);
+        bool operator==(const string& hash) const;
+
+        const optional<TxType>& GetType() const;
+        void SetType(TxType value);
+
+        const optional<int64_t>& GetVersion() const;
+        void SetVersion(int64_t value);
+
+        const optional<int64_t>& GetTime() const;
+        void SetTime(int64_t value);
+
+        const optional<int64_t>& GetLockTime() const;
+        void SetLockTime(int64_t value);
+
+        const optional<int64_t>& GetHeight() const;
+        void SetHeight(int64_t value);
+
+        const optional<string>& GetBlockHash() const;
+        void SetBlockHash(string value);
+
+        const optional<bool>& GetLast() const;
+        void SetLast(bool value);
+
+        const optional<bool>& GetFirst() const;
+        void SetFirst(bool value);
+
+        const optional<int64_t>& GetId() const;
+        void SetId(int64_t value);
+
+        const optional<string>& GetString1() const;
+        void SetString1(string value);
+
+        const optional<string>& GetString2() const;
+        void SetString2(string value);
+
+        const optional<string>& GetString3() const;
+        void SetString3(string value);
+
+        const optional<string>& GetString4() const;
+        void SetString4(string value);
+
+        const optional<string>& GetString5() const;
+        void SetString5(string value);
+
+        const optional<int64_t>& GetInt1() const;
+        void SetInt1(int64_t value);
+
+        vector<TransactionInput>& Inputs();
+        vector<TransactionOutput>& Outputs();
+        const vector<TransactionOutput>& OutputsConst() const;
+
+        optional<Payload>& GetPayload();
+        const optional<Payload>& GetPayload() const;
+        void SetPayload(Payload value);
+        bool HasPayload() const;
+        
+        void GeneratePayload();
+        void ClearPayload();
+        virtual size_t PayloadSize() const;
+
+    protected:
+        optional<string> m_hash = nullopt;
+        optional<TxType> m_type = nullopt;
+        optional<int64_t> m_version = nullopt;
+        optional<int64_t> m_time = nullopt;
+        optional<int64_t> m_locktime = nullopt;
+        optional<int64_t> m_height = nullopt;
+        optional<string> m_blockhash = nullopt;
+        optional<bool> m_last = nullopt;
+        optional<bool> m_first = nullopt;
+        optional<int64_t> m_id = nullopt;
+        optional<string> m_string1 = nullopt;
+        optional<string> m_string2 = nullopt;
+        optional<string> m_string3 = nullopt;
+        optional<string> m_string4 = nullopt;
+        optional<string> m_string5 = nullopt;
+        optional<int64_t> m_int1 = nullopt;
+        optional<Payload> m_payload = nullopt;
+        vector<TransactionInput> m_inputs;
+        vector<TransactionOutput> m_outputs;
+    };
+
+} // namespace PocketTx
+
+#endif // POCKETTX_BLOCK_H
