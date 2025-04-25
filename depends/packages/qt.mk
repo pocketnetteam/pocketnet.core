f66c72f5ab73170356cbb509a0f2a9c52422727c
Andy Oknen <andy.oknen@yandex.ru>
2025-04-25 14:54:32 +0800

    Revert "chore: Add -no-feature-schannel and -openssl-linked options to Qt configuration for MinGW builds"
    This reverts commit b84314176b4bafdaf68c77628a135d6dcaea9230.

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -182,8 +182,6 @@ $(package)_config_opts_mingw32 += "QMAKE_LFLAGS = '$($(package)_ldflags)'"
 $(package)_config_opts_mingw32 += "QMAKE_LIB = '$($(package)_ar) rc'"
 $(package)_config_opts_mingw32 += -device-option CROSS_COMPILE="$(host)-"
 $(package)_config_opts_mingw32 += -pch
-$(package)_config_opts_mingw32 += -no-feature-schannel
-$(package)_config_opts_mingw32 += -openssl-linked
 ifneq ($(LTO),)
 $(package)_config_opts_mingw32 += -ltcg
 endif
