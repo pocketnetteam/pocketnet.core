03d8657b194b84de9619eb90b303bf4e38e6aea6
Andy Oknen <andy.oknen@yandex.ru>
2025-04-28 09:05:14 +0800

    feat: add function to abandon orphaned coinstakes in wallet management

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -922,6 +922,8 @@ public:
     void transactionAddedToMempool(const CTransactionRef& tx, uint64_t mempool_sequence) override;
     void blockConnected(const CBlock& block, int height) override;
     void blockDisconnected(const CBlock& block, int height) override;
+    /* Function that will remove potentially abandoned coinstakes, returning the input to the wallet. */
+    void AbandonOrphanedCoinstakes();
     void updatedBlockTip() override;
     int64_t RescanFromTime(int64_t startTime, const WalletRescanReserver& reserver, bool update);
 
