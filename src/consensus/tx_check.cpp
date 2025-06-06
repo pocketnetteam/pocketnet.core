bd43d4b06d51c75856f38b0347af414d2914db3f
Andy Oknen <andy.oknen@yandex.ru>
2025-06-06 13:46:28 +0800

    refactor: fix copyrights for files created bitcoin team

--- src/consensus/tx_check.cpp
+++ src/consensus/tx_check.cpp
@@ -1,4 +1,5 @@
-// Copyright (c) 2017-2019 The Pocketcoin Core developers
+// Copyright (c) 2017-2019 The Bitcoin Core developers
+// Copyright (c) 2019-2025 The Pocketcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
