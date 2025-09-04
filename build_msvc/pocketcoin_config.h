df74d6a9226ca8319bb53f4a684e908dcbf6089e
Andy Oknen <andy.oknen@yandex.ru>
2025-09-04 18:30:24 +0800

    Update version to 0.22.19 and adjust minimum chain work and assume valid checkpoints in chainparams.

--- build_msvc/pocketcoin_config.h
+++ build_msvc/pocketcoin_config.h
@@ -16,7 +16,7 @@
 #define CLIENT_VERSION_MINOR 22
 
 /* Build revision */
-#define CLIENT_VERSION_REVISION 18
+#define CLIENT_VERSION_REVISION 19
 
 /* Version Build */
 #define CLIENT_VERSION_BUILD 0
@@ -348,7 +348,7 @@
 #define PACKAGE_NAME "Pocketnet Core"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Pocketnet Core 0.22.18"
+#define PACKAGE_STRING "Pocketnet Core 0.22.19"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "pocketcoin"
@@ -357,7 +357,7 @@
 #define PACKAGE_URL "https://github.com/pocketnetteam"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.22.18"
+#define PACKAGE_VERSION "0.22.19"
 
 /* Define to necessary symbol if this constant uses a non-standard name on
    your system. */
